a1c2a3e1cfb2e34ee20ec7ca79006308dbeb4fb2
Mohsin Ahmed Shaikh <mohsinshaikh786@gmail.com>
2015-10-07 16:46:39 +0800

    Implemented OMP_MASTER instead of OMP_SINGLE and a BARRIER for reading in the total number of threads up on which the calculation for decomposition depends.

--- code/submit.sh
+++ code/submit.sh
@@ -6,8 +6,18 @@
 #SBATCH --partition=debugq
 #SBATCH --time=00:30:00
 #SBATCH --export=NONE
-export OMP_NUM_THREADS=4
-INPATH=../Inputs_50KM
-OUTPATH=../output
+INPATH=../Inputs_01_12
+OUTPATH=../rthd_1
+if [ ! -d ${OUTPATH} ]; then
+	mkdir ${OUTPATH}
+fi 
+export OMP_NUM_THREADS=1
 time -p aprun -n 1 -N 1 -d ${OMP_NUM_THREADS} ./a.out 1 ${INPATH} ${OUTPATH}
 
+
+OUTPATH=../output
+if [ ! -d ${OUTPATH} ]; then
+	mkdir ${OUTPATH}
+fi 
+export OMP_NUM_THREADS=4
+time -p aprun -n 1 -N 1 -d ${OMP_NUM_THREADS} ./a.out 1 ${INPATH} ${OUTPATH}
