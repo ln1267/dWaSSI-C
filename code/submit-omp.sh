fe6533b21cbd0b57c7e71a9fe1a7bf2d6706ffef
Mohsin Ahmed Shaikh <mohsinshaikh786@gmail.com>
2015-10-12 13:14:12 +0800

    Adding profiling script and reports for omp ver 2

---
+++ code/submit-omp.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -l
+#SBATCH --job-name=WaSSI
+#SBATCH --account=director100
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --partition=workq
+#SBATCH --time=00:30:00
+#SBATCH --export=NONE
+INPATH=../Inputs_50KM
+OUTPATH=../rthd_1
+if [ ! -d ${OUTPATH} ]; then
+	mkdir ${OUTPATH}
+fi 
+export OMP_NUM_THREADS=1
+time -p aprun -n 1 -N 1 -d ${OMP_NUM_THREADS} ./a.out+pat 1 ${INPATH} ${OUTPATH}
+
+
+OUTPATH=../rthd_2
+if [ ! -d ${OUTPATH} ]; then
+	mkdir ${OUTPATH}
+fi 
+export OMP_NUM_THREADS=2
+time -p aprun -n 1 -N 1 -d ${OMP_NUM_THREADS} ./a.out+pat 1 ${INPATH} ${OUTPATH}
+
+OUTPATH=../rthd_4
+if [ ! -d ${OUTPATH} ]; then
+	mkdir ${OUTPATH}
+fi 
+export OMP_NUM_THREADS=4
+time -p aprun -n 1 -N 1 -d ${OMP_NUM_THREADS} ./a.out+pat 1 ${INPATH} ${OUTPATH}
+
+OUTPATH=../rthd_8
+if [ ! -d ${OUTPATH} ]; then
+	mkdir ${OUTPATH}
+fi 
+export OMP_NUM_THREADS=8
+time -p aprun -n 1 -N 1 -d ${OMP_NUM_THREADS} ./a.out+pat 1 ${INPATH} ${OUTPATH}
+
+OUTPATH=../rthd_16
+if [ ! -d ${OUTPATH} ]; then
+	mkdir ${OUTPATH}
+fi 
+export OMP_NUM_THREADS=16
+time -p aprun -n 1 -N 1 -d ${OMP_NUM_THREADS} ./a.out+pat 1 ${INPATH} ${OUTPATH}
+
+
