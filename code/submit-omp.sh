64f3f0cdc9682eff99c861674010d797770642c1
Ning <ln1267@gmail.com>
2015-10-22 20:02:24 +0800

    Modify sub-opm for ning

--- code/submit-omp.sh
+++ code/submit-omp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 #SBATCH --job-name=WaSSI
-#SBATCH --account=director100
+#SBATCH --account=director1234
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --partition=workq
@@ -12,7 +12,7 @@ if [ ! -d ${OUTPATH} ]; then
 	mkdir ${OUTPATH}
 fi 
 export OMP_NUM_THREADS=1
-time -p aprun -n 1 -N 1 -d ${OMP_NUM_THREADS} ./a.out+pat 1 ${INPATH} ${OUTPATH}
+time -p aprun -n 1 -N 1 -d ${OMP_NUM_THREADS} ./a.out 1 ${INPATH} ${OUTPATH}
 
 
 OUTPATH=/group/director1234/nliu/rthd_2
@@ -20,27 +20,27 @@ if [ ! -d ${OUTPATH} ]; then
 	mkdir ${OUTPATH}
 fi 
 export OMP_NUM_THREADS=2
-time -p aprun -n 1 -N 1 -d ${OMP_NUM_THREADS} ./a.out+pat 1 ${INPATH} ${OUTPATH}
+time -p aprun -n 1 -N 1 -d ${OMP_NUM_THREADS} ./a.out 1 ${INPATH} ${OUTPATH}
 
 OUTPATH=/group/director1234/nliu/rthd_4
 if [ ! -d ${OUTPATH} ]; then
 	mkdir ${OUTPATH}
 fi 
 export OMP_NUM_THREADS=4
-time -p aprun -n 1 -N 1 -d ${OMP_NUM_THREADS} ./a.out+pat 1 ${INPATH} ${OUTPATH}
+time -p aprun -n 1 -N 1 -d ${OMP_NUM_THREADS} ./a.out 1 ${INPATH} ${OUTPATH}
 
 OUTPATH=/group/director1234/nliu/rthd_8
 if [ ! -d ${OUTPATH} ]; then
 	mkdir ${OUTPATH}
 fi 
 export OMP_NUM_THREADS=8
-time -p aprun -n 1 -N 1 -d ${OMP_NUM_THREADS} ./a.out+pat 1 ${INPATH} ${OUTPATH}
+time -p aprun -n 1 -N 1 -d ${OMP_NUM_THREADS} ./a.out 1 ${INPATH} ${OUTPATH}
 
 OUTPATH=/group/director1234/nliu/rthd_16
 if [ ! -d ${OUTPATH} ]; then
 	mkdir ${OUTPATH}
 fi 
 export OMP_NUM_THREADS=16
-time -p aprun -n 1 -N 1 -d ${OMP_NUM_THREADS} ./a.out+pat 1 ${INPATH} ${OUTPATH}
+time -p aprun -n 1 -N 1 -d ${OMP_NUM_THREADS} ./a.out 1 ${INPATH} ${OUTPATH}
 
 
