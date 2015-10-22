3ff8812a461dea2d2153a2d936d67873427ea991
Ning <ln1267@gmail.com>
2015-10-22 19:53:46 +0800

    Change out directory to

--- code/submit-omp.sh
+++ code/submit-omp.sh
@@ -6,8 +6,8 @@
 #SBATCH --partition=workq
 #SBATCH --time=00:30:00
 #SBATCH --export=NONE
-INPATH=../Inputs_50KM
-OUTPATH=../rthd_1
+INPATH=../INPUTS_20KM
+OUTPATH=/group/director1234/nliu/rthd_1
 if [ ! -d ${OUTPATH} ]; then
 	mkdir ${OUTPATH}
 fi 
@@ -15,28 +15,28 @@ export OMP_NUM_THREADS=1
 time -p aprun -n 1 -N 1 -d ${OMP_NUM_THREADS} ./a.out+pat 1 ${INPATH} ${OUTPATH}
 
 
-OUTPATH=../rthd_2
+OUTPATH=/group/director1234/nliu/rthd_2
 if [ ! -d ${OUTPATH} ]; then
 	mkdir ${OUTPATH}
 fi 
 export OMP_NUM_THREADS=2
 time -p aprun -n 1 -N 1 -d ${OMP_NUM_THREADS} ./a.out+pat 1 ${INPATH} ${OUTPATH}
 
-OUTPATH=../rthd_4
+OUTPATH=/group/director1234/nliu/rthd_4
 if [ ! -d ${OUTPATH} ]; then
 	mkdir ${OUTPATH}
 fi 
 export OMP_NUM_THREADS=4
 time -p aprun -n 1 -N 1 -d ${OMP_NUM_THREADS} ./a.out+pat 1 ${INPATH} ${OUTPATH}
 
-OUTPATH=../rthd_8
+OUTPATH=/group/director1234/nliu/rthd_8
 if [ ! -d ${OUTPATH} ]; then
 	mkdir ${OUTPATH}
 fi 
 export OMP_NUM_THREADS=8
 time -p aprun -n 1 -N 1 -d ${OMP_NUM_THREADS} ./a.out+pat 1 ${INPATH} ${OUTPATH}
 
-OUTPATH=../rthd_16
+OUTPATH=/group/director1234/nliu/rthd_16
 if [ ! -d ${OUTPATH} ]; then
 	mkdir ${OUTPATH}
 fi 
