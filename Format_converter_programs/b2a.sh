ec12c546dcec3f881f5eb9414d626c2501954cfa
ning <ln1267@gmail.com>
2016-02-12 10:59:01 +0800

    correct b2a.sh

--- Format_converter_programs/b2a.sh
+++ Format_converter_programs/b2a.sh
@@ -7,11 +7,11 @@ let LINE_MONTH=$LINE_YEARS*$LINE_CELLS*12
 echo $directory $LINE_YEARS $LINE_CELLS $LINE_ANNUAL $LINE_MONTH
 
 #./bin2ascii -i $directory/VEGINFO.DAT -o $directory/VEGINFO_1.TXT -m 6772800 -n 3
-./ascii2bin -i $directory/HUCFLOW.DAT -o $directory/HUCFLOW.TXT -m $LINE_CELLS -n 9
-./ascii2bin -i $directory/HUCCARBON.DAT -o $directory/HUCCARBON.TXT -m $LINE_CELLS -n 5
-./ascii2bin -i $directory/ANNUALFLOW.DAT -o $directory/ANNUALFLOW.TXT -m $LINE_ANNUAL -n 12
-./ascii2bin -i $directory/ANNUALCARBON.DAT -o $directory/ANNUALCARBON.TXT -m $LINE_ANNUAL -n 7
-./ascii2bin -i $directory/MONTHFLOW.DAT -o $directory/MONTHFLOW.TXT -m $LINE_MONTH -n 13
-./ascii2bin -i $directory/MONTHCARBON.DAT -o $directory/MONTHCARBON.TXT -m $LINE_MONTH -n 6
-./ascii2bin -i $directory/SOILSTORAGE.DAT -o $directory/SOILSTORAGE.TXT -m $LINE_MONTH -n 8
-./ascii2bin -i $directory/VALIDATION.DAT -o $directory/VALIDATION.TXT -m $LINE_MONTH -n 18
+./bin2ascii -i $directory/HUCFLOW.DAT -o $directory/HUCFLOW.TXT -m $LINE_CELLS -n 9
+./bin2ascii -i $directory/HUCCARBON.DAT -o $directory/HUCCARBON.TXT -m $LINE_CELLS -n 5
+./bin2ascii -i $directory/ANNUALFLOW.DAT -o $directory/ANNUALFLOW.TXT -m $LINE_ANNUAL -n 12
+./bin2ascii -i $directory/ANNUALCARBON.DAT -o $directory/ANNUALCARBON.TXT -m $LINE_ANNUAL -n 7
+./bin2ascii -i $directory/MONTHFLOW.DAT -o $directory/MONTHFLOW.TXT -m $LINE_MONTH -n 13
+./bin2ascii -i $directory/MONTHCARBON.DAT -o $directory/MONTHCARBON.TXT -m $LINE_MONTH -n 6
+./bin2ascii -i $directory/SOILSTORAGE.DAT -o $directory/SOILSTORAGE.TXT -m $LINE_MONTH -n 8
+./bin2ascii -i $directory/VALIDATION.DAT -o $directory/VALIDATION.TXT -m $LINE_MONTH -n 18
