f4c5acfc719e177e259f2d94cb5e8b7c94ee13c4
Ning <ln1267@gmail.com>
2016-02-12 09:55:37 +0800

    update format converter's dash

--- Format_converter_programs/a2b.sh
+++ Format_converter_programs/a2b.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-director="/group/y98/nliu/WaSSI_AU/INPUTS"
-echo $director
+directory="/group/y98/nliu/WaSSI_AU/INPUTS"
+LINE_YEARS=14
+LINE_CELLS=564400
+let LINE_ANNUAL=$LINE_YEARS*$LINE_CELLS
+let LINE_MONTH=$LINE_YEARS*$LINE_CELLS*12
+echo $directory $LINE_YEARS $LINE_CELLS $LINE_ANNUAL $LINE_MONTH
 
-#./bin2ascii -i $director/VEGINFO.DAT -o $director/VEGINFO_1.TXT -m 6772800 -n 3
-./ascii2bin -i $director/CLIMATE.TXT -o $director/CLIMATE.DAT -m 94819200 -n 5
-./ascii2bin -i $director/LANDLAI.TXT -o $director/LANDLAI.DAT -m 94819200 -n 4
-./ascii2bin -i $director/SOILINFO.TXT -o $director/SOILINFO.DAT -m 564400 -n 12
-./ascii2bin -i $director/CELLINFO.TXT -o $director/CELLINFO.DAT -m 6772800 -n 5
-./ascii2bin -i $director/VEGINFO.TXT -o $director/VEGINFO.DAT -m 6772800 -n 3
+echo $directory
+
+#./bin2ascii -i $directory/VEGINFO.DAT -o $directory/VEGINFO_1.TXT -m 6772800 -n 3
+./ascii2bin -i $directory/CLIMATE.TXT -o $directory/CLIMATE.DAT -m $LINE_MONTH -n 5
+./ascii2bin -i $directory/LANDLAI.TXT -o $directory/LANDLAI.DAT -m $LINE_MONTH -n 4
+./ascii2bin -i $directory/SOILINFO.TXT -o $directory/SOILINFO.DAT -m $LINE_CELLS -n 12
+./ascii2bin -i $directory/CELLINFO.TXT -o $directory/CELLINFO.DAT -m $LINE_CELLS -n 5
+./ascii2bin -i $directory/VEGINFO.TXT -o $directory/VEGINFO.DAT -m 6772800 -n 3
--- Format_converter_programs/b2a.sh
+++ Format_converter_programs/b2a.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-director="/group/y98/nliu/WaSSI_AU/OUTPUTS"
-echo $director
+directory="/group/y98/nliu/WaSSI_AU/OUTPUTS"
+LINE_YEARS=14
+LINE_CELLS=564400
+let LINE_ANNUAL=$LINE_YEARS*$LINE_CELLS
+let LINE_MONTH=$LINE_YEARS*$LINE_CELLS*12
+echo $directory $LINE_YEARS $LINE_CELLS $LINE_ANNUAL $LINE_MONTH
 
-#./bin2ascii -i $director/VEGINFO.DAT -o $director/VEGINFO_1.TXT -m 6772800 -n 3
-./bin2ascii -i $director/CLIMATE.TXT -o $director/CLIMATE.DAT -m 94819200 -n 5
-./ascii2bin -i $director/LANDLAI.TXT -o $director/LANDLAI.DAT -m 94819200 -n 4
-./ascii2bin -i $director/SOILINFO.TXT -o $director/SOILINFO.DAT -m 564400 -n 12
-./ascii2bin -i $director/CELLINFO.TXT -o $director/CELLINFO.DAT -m 6772800 -n 5
-./ascii2bin -i $director/VEGINFO.TXT -o $director/VEGINFO.DAT -m 6772800 -n 3
+#./bin2ascii -i $directory/VEGINFO.DAT -o $directory/VEGINFO_1.TXT -m 6772800 -n 3
+./ascii2bin -i $directory/HUCFLOW.DAT -o $directory/HUCFLOW.TXT -m $LINE_CELLS -n 9
+./ascii2bin -i $directory/HUCCARBON.DAT -o $directory/HUCCARBON.TXT -m $LINE_CELLS -n 5
+./ascii2bin -i $directory/ANNUALFLOW.DAT -o $directory/ANNUALFLOW.TXT -m $LINE_ANNUAL -n 12
+./ascii2bin -i $directory/ANNUALCARBON.DAT -o $directory/ANNUALCARBON.TXT -m $LINE_ANNUAL -n 7
+./ascii2bin -i $directory/MONTHFLOW.DAT -o $directory/MONTHFLOW.TXT -m $LINE_MONTH -n 13
+./ascii2bin -i $directory/MONTHCARBON.DAT -o $directory/MONTHCARBON.TXT -m $LINE_MONTH -n 6
+./ascii2bin -i $directory/SOILSTORAGE.DAT -o $directory/SOILSTORAGE.TXT -m $LINE_MONTH -n 8
+./ascii2bin -i $directory/VALIDATION.DAT -o $directory/VALIDATION.TXT -m $LINE_MONTH -n 18
